c5c07a4a4f9f369f4ccaf354e8f931b448b50572
isengupt <ishan6060@gmail.com>
2020-08-26 15:04:37 -0400

    Edited graph, added lines, and create color code

--- subplots.py
+++ subplots.py
@@ -3,15 +3,13 @@ import matplotlib.pyplot as plt
 from matplotlib.patches import Circle
 from matplotlib import animation
 from itertools import combinations
+import random
 
 class Particle:
-    """A class representing a two-dimensional particle."""
+    """A generic person with attributes in the world simulation"""
 
     def __init__(self, x, y, vx, vy, radius):
-        """Initialize the particle's position, velocity, and radius.
-
-        Any key-value pairs passed in the styles dictionary will be passed
-        as arguments to Matplotlib's Circle patch constructor.
+        """Initialize the particle's pos, vel, and susceptiability and state factors
 
         """
 
@@ -26,11 +24,7 @@ class Particle:
         self.timeStateStart = 0
 
 
-       
-     
 
-    # For convenience, map the components of the particle's position and
-    # velocity vector onto the attributes x, y, vx and vy.
     @property
     def x(self):
         return self.r[0]
@@ -72,11 +66,11 @@ class Particle:
     
     
     def sampleCovidState(self):   
-        initialState  = "infected" if np.random.uniform(low=0.0, high=1.0) < 0.05 else "susceptible"
+        initialState  = "infected" if random.random() < 0.05 else "susceptible"
         if initialState == "infected":
-            self.styles = {'color': 'red'}
+            self.styles = {'color': '#F07C4E'}
         else:
-            self.styles = {'color': 'green'}
+            self.styles = {'color': '#B0C4DE'}
         return initialState
     
     def sampleSusceptibility(self):
@@ -124,18 +118,20 @@ class World:
     def __init__(self, n, radius, ticksPerDay):     
         self.compartmentStats = {"latent":0, "asymptomatic":0, "infected":0, "susceptible": 0, "recovered":0, "dead":0}
         self.init_particles(n, radius)
+        
         self.curTime = 0
         self.ticksPerDay = ticksPerDay
-        self.completeInteraction = []
         self.timeInterval = []
         self.recoveryInterval = []
         self.deathInterval = []
+        self.asymptomaticInterval = []
+        self.susceptibleInterval = []
+        self.latentInterval = []
+        self.infectedInterval = []
 
     def init_particles(self, n, radius):
-        """Initialize the n Particles of the simulation.
-
-        Positions and velocities are chosen randomly; radius can be a single
-        value or a sequence with n values.
+        """Intialize particles from particle class with random pos, vel, 
+        and a state
 
         """
 
@@ -144,46 +140,39 @@ class World:
         
         while (len(self.particles) < n):
     
-                # Choose x, y so that the Particle is entirely inside the
-                # domain of the simulation.
+        
                 x, y = radius + (1 - 2*radius) * np.random.random(2)
-                # Choose a random velocity (within some reasonable range of
-                # values) for the Particle.
+               
+        
                 vr = 0.1 * np.random.random() + 0.05
                 vphi = 2*np.pi * np.random.random()
                 vx, vy = vr * np.cos(vphi), vr * np.sin(vphi)
+              
                 particle = Particle(x, y, vx, vy, radius)
              
-                # Check that the Particle doesn't overlap one that's already
-                # been placed.
+                """Removes overlapping particles """
                 for p2 in self.particles:
                     if p2.overlaps(particle):
                         continue
                 else:
-                   # print(particle.susceptibility)
-                    #print(particle.covidState)
-                    #print(particle.infectivity)
+                 
                     self.compartmentStats[particle.covidState] += 1
-                    #print(self.compartmentStats)
+                    
                     self.particles.append(particle)
 
-    @property
-    def getCompartments(self):
-        return self.compartmentStats
+
+
 
     def handle_collisions(self):
 
-        """Detect and handle any collisions between the Particles.
-
-        When two Particles collide, they do so elastically: their velocities
-        change such that both energy and momentum are conserved.
+        """Function to handle collisions and allocate a state based on covidState of interacting
+        particles
 
         """
 
         def change_velocities(p1, p2):
             """
-            Particles p1 and p2 have collided elastically: update their
-            velocities.
+            Use momentum and velocity to pick decide velocity of two particles after ellastic collision
 
             """
 
@@ -197,9 +186,7 @@ class World:
             p1.v = u1
             p2.v = u2
 
-        # We're going to need a sequence of all of the pairs of particles when
-        # we are detecting collisions. combinations generates pairs of indexes
-        # into the self.particles list of Particles on the fly.
+        """ Create pairs to evaluate whether any two overlapped """ 
         pairs = combinations(range(self.n), 2)
         for i,j in pairs:
             if self.particles[i].overlaps(self.particles[j]):
@@ -209,19 +196,21 @@ class World:
                 
                 change_velocities(self.particles[i], self.particles[j])
 
-                if ((self.particles[i].covidState == "infected" or self.particles[i].covidState == "asymptotic") and self.particles[j].covidState == "susceptible") :
-                        temp = np.random.uniform(low=0.0, high=1.0)
+                if ((self.particles[i].covidState == "infected" or self.particles[i].covidState == "asymptomatic") and self.particles[j].covidState == "susceptible") :
+                        temp = random.random()
                         if (temp < self.particles[i].infectivity * self.particles[j].susceptibility):
                             self.particles[j].covidState = "latent"
+                            self.particles[j].styles = {'color': '#FAD9B9'}
                             self.particles[j].timeStateStart = self.curTime
-                            self.compartmentStats['latent'] += 1
-                   
+                            self.compartmentStats['latent'] += 1  
                             self.compartmentStats['susceptible'] -= 1
                 
-                if ((self.particles[j].covidState == "infected" or self.particles[j].covidState == "asymptotic") and self.particles[i].covidState == "susceptible") :
-                        temp = np.random.uniform(low=0.0, high=1.0)
+                if ((self.particles[j].covidState == "infected" or self.particles[j].covidState == "asymptomatic") and self.particles[i].covidState == "susceptible") :
+                        temp = random.random()
                         if (temp < self.particles[j].infectivity * self.particles[i].susceptibility):
+
                             self.particles[i].covidState = "latent"
+                            self.particles[i].styles = {'color': '#FAD9B9'}
                             self.particles[i].timeStateStart = self.curTime
                             self.compartmentStats['latent'] += 1
                             self.compartmentStats['susceptible'] -= 1
@@ -229,6 +218,7 @@ class World:
                 
     @staticmethod
     def sampleRecovery(infectiontime):
+        """ Returns probability of recovery at certain point of time in infection state """
         minInfectionTime = 14
         if (infectiontime < minInfectionTime):
             return 0
@@ -238,7 +228,7 @@ class World:
     
     @staticmethod
     def sampleDeath(infectiontime):
-
+        """ Returns porbability of death with input as time infected """
         minInfectionTime = 17
         heuristic = 30.0
         if (infectiontime < 14):
@@ -248,6 +238,8 @@ class World:
 
     @staticmethod
     def sampleInfection(timeLatent):
+        """ Returns infection probability and requires time patient was latent as input
+        """ 
         if (timeLatent < 2):
             return 0
         else:
@@ -255,11 +247,11 @@ class World:
             return (timeLatent - 2.0) / heuristic
     
     @staticmethod
+
     def sampleAsymptotic():
+        """ Returns probability of being asymptomatic """
         return 0.1                    
-    
-    
-    
+        
     def advance_animation(self, dt):
         for i, p in enumerate(self.particles):
             temp = np.random.uniform(low=0.0, high=1.0)
@@ -271,7 +263,7 @@ class World:
                 if (temp < self.sampleRecovery(stateTimeDays)/self.ticksPerDay):
                     self.compartmentStats[p.covidState] -= 1
                     self.compartmentStats["recovered"] += 1
-                    p.styles = {'color': 'purple'}
+                    p.styles = {'color': '#9370DB'}
                     p.covidState = "recovered"
                     p.timeStateStart = self.curTime
                 
@@ -279,7 +271,7 @@ class World:
                 elif (temp2 < self.sampleDeath(stateTimeDays) / self.ticksPerDay):
                     self.compartmentStats[p.covidState] -= 1
                     p.covidState = "dead"
-                    p.styles = {'color': 'black'}
+                    p.styles = {'color': '#000000'}
                     self.compartmentStats["dead"] += 1
                     p.timeStateStart = self.curTime
               
@@ -291,11 +283,11 @@ class World:
                     self.compartmentStats["latent"] -= 1
                     if (temp2 < self.sampleAsymptotic()):
                         p.covidState = "asymptomatic"
-                        p.styles = {'color': 'green'}
+                        p.styles = {'color': '#F39F79'}
                         self.compartmentStats["asymptomatic"] += 1
                     else:
                         p.covidState = "infected"
-                        p.styles = {'color': 'red'}
+                        p.styles = {'color': '#F07C4E'}
                         self.compartmentStats["infected"] += 1
 
             if (p.covidState != "dead"):
@@ -312,9 +304,15 @@ class World:
 
 
     def init(self):
-        """Initialize the Matplotlib animation."""
+        """ Initialize the lines and circles with empty arrays and initial positions 
+        respectively
+        """
         self.lineDeathes.set_data([], [])
         self.lineRecovered.set_data([], [])
+        self.lineAsymptotic.set_data([], [])
+        self.lineSusceptible.set_data([],[])
+        self.lineLatent.set_data([],[])
+        self.lineInfected.set_data([],[])
         self.circles = []
         for particle in self.particles:
             self.circles.append(particle.draw(self.ax))
@@ -322,30 +320,31 @@ class World:
 
 
     def animate(self, i):
-        """The function passed to Matplotlib's FuncAnimation routine."""
+        """Animation for just moving the particles forward"""
         self.curTime += 1
         self.advance_animation(0.1)
-        #self.completeInteraction.append(self.compartmentStats)
+     
         return self.circles
 
-
-    def update(self, i, l1, l2):
-        self.timeInterval.append(self.curTime)
-        self.recoveryInterval.append(self.compartmentStats['recovered'])
-        self.deathInterval.append(self.compartmentStats['dead'])
-
-        l1.set_data(self.timeInterval, self.recoveryInterval)
-        l2.set_data(self.timeInterval, self.deathInterval)
-        self.curTime += 1
-        self.advance_animation(1)
-        return [l1,l2]
-
     def animateAll(self, i):
+        
         self.timeInterval.append(self.curTime)
         self.recoveryInterval.append(self.compartmentStats['recovered'])
         self.deathInterval.append(self.compartmentStats['dead'])
+        self.asymptomaticInterval.append(self.compartmentStats['asymptomatic'])
+        self.susceptibleInterval.append(self.compartmentStats['susceptible'])
+        self.latentInterval.append(self.compartmentStats['latent'])
+        self.infectedInterval.append(self.compartmentStats['infected'])
+        
         self.lineRecovered.set_data(self.timeInterval, self.recoveryInterval)
         self.lineDeathes.set_data(self.timeInterval, self.deathInterval)
+        self.lineSusceptible.set_data(self.timeInterval, self.susceptibleInterval)
+        self.lineLatent.set_data(self.timeInterval, self.latentInterval)
+        self.lineAsymptotic.set_data(self.timeInterval, self.asymptomaticInterval)
+        self.lineInfected.set_data(self.timeInterval, self.infectedInterval)
+        
+        self.ax_line.set_xlim(0, max(self.timeInterval))
+        
         
         self.curTime += 1
         self.advance_animation(0.1)
@@ -355,69 +354,48 @@ class World:
     
 
     def do_animation(self, save=False):
-        """Set up and carry out the animation of the molecular dynamics.
-
-        To save the animation as a MP4 movie, set save=True.
+        """Set up lines subplot and animation for particles side-by-side and give
+        saving options
         """
 
         
         fig =  plt.figure()
-        self.ax = fig.add_subplot(2,2,1)
+        plt.tight_layout()
+        self.ax = fig.add_subplot(221)
         for s in ['top','bottom','left','right']:
             self.ax.spines[s].set_linewidth(2)
         self.ax.set_aspect('equal', 'box')
+       
         self.ax.set_xlim(0, 1)
         self.ax.set_ylim(0, 1)
         self.ax.xaxis.set_ticks([])
         self.ax.yaxis.set_ticks([])
-        self.ax_line = fig.add_subplot(1,2,2)
-        self.ax_line.set_ylim([0,100])
-        self.ax_line.set_xlim([0,500])
-     
+        self.ax_line = fig.add_subplot(222)
+        self.ax_line.set_ylim(0, self.n)
+
+        self.lineSusceptible = self.ax_line.plot([],[], color="#B0C4DE", label="susceptible")[0]
+        self.lineLatent = self.ax_line.plot([],[], color="#FAD9B9", label="latent")[0]
+        self.lineAsymptotic = self.ax_line.plot([],[], color="#F39F79", label="asympotmatic")[0]
+        self.lineInfected = self.ax_line.plot([],[], color="#F07C4E" ,label="infected")[0]
+        self.lineRecovered = self.ax_line.plot([],[], color ="#9370DB", label="recovered")[0]
+        self.lineDeathes = self.ax_line.plot([],[], color="#000000", label="dead")[0]
+
+        self.patches = [self.ax, self.lineSusceptible, self.lineLatent, self.lineAsymptotic, self.lineRecovered, self.lineDeathes]
         
-        self.lineRecovered = self.ax_line.plot([],[], color ="r")[0]
-        self.lineDeathes = self.ax_line.plot([],[], color="g")[0]
-
-        self.patches = [self.ax, self.lineRecovered, self.lineDeathes]
-
         anim = animation.FuncAnimation(fig, self.animateAll, init_func=self.init,
-                               frames=100, interval=20, blit=True)
-
-
-
-
-
-        """  self.ax = fig.add_subplot(1,2,2)
-        self.ax_line = fig.add_subplot(2,2,1)
-        l1, = self.ax_line.plot([], [], color = "r")
-        l2, = self.ax_line.plot([], [], color = "g")
-
-
-        self.ax_line.set_ylim([0,100])
-        self.ax_line.set_xlim([0,500])
-        for s in ['top','bottom','left','right']:
-            self.ax.spines[s].set_linewidth(2)
-        self.ax.set_aspect('equal', 'box')
-        self.ax.set_xlim(0, 1)
-        self.ax.set_ylim(0, 1)
-        self.ax.xaxis.set_ticks([])
-        self.ax.yaxis.set_ticks([])
-        anim = animation.FuncAnimation(fig, self.animate, init_func=self.init,
-                               frames=100, interval=2, blit=True)
-        ani = animation.FuncAnimation(fig, self.update, 100, fargs=[l1,l2],
-                  interval=2, blit=True) """
+                               frames=100, interval=20, blit=False)
         
         if save:
             Writer = animation.writers['ffmpeg']
             writer = Writer(fps=100, bitrate=1800)
-            anim.save('collision.mp4', writer=writer)
+            anim.save('CovidModel.mp4', writer=writer)
         else:
+            plt.legend()
             plt.show()
                     
 
 
 if __name__ == '__main__':
     nparticles = 100
-    #radii = np.random.random(nparticles)*0.03+0.02
     sim = World(nparticles, 0.01, 10)
     sim.do_animation(save=False)
